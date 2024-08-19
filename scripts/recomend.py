7bfa1b7b2797c87f231409742827197045c1cf67
Micz26 <mikolajczachorowski260203@gmail.com>
2024-08-19 19:25:08 +0200

    add load_dotenv() in recomend.py, fix pyproject.toml

--- scripts/recomend.py
+++ scripts/recomend.py
@@ -2,21 +2,24 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-import faiss
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAI
 import fire
 import os
-import pickle
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 vectorstore_path = "./vectorstores/test_vectorstore"
 embeddings = OpenAIEmbeddings()
 
 if os.path.exists(vectorstore_path):
-    db = FAISS.load_local(vectorstore_path, embeddings, allow_dangerous_deserialization=True)
+    db = FAISS.load_local(
+        vectorstore_path, embeddings, allow_dangerous_deserialization=True
+    )
 else:
-    loader = CSVLoader(file_path="./data/test_staff_data.csv", encoding="utf-8")
+    loader = CSVLoader(file_path="./data/papers_data.csv", encoding="utf-8")
     data = loader.load()
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -26,11 +29,11 @@ else:
     db.save_local(vectorstore_path)
 
 
-retriever = db.as_retriever(search_kwargs={'k': 5})
+retriever = db.as_retriever(search_kwargs={"k": 5})
 
 qa_chain = RetrievalQA.from_chain_type(
     llm=OpenAI(temperature=0.2),
-    chain_type='stuff',
+    chain_type="stuff",
     retriever=retriever,
     return_source_documents=True,
 )
@@ -50,8 +53,8 @@ def main(
     question: str = "Who should I pick as promotor for my engineering thesis 'Train Rescheduling and Track Closure Optimization'",
 ):
     formatted_prompt = PROMPT_TEMPLATE.format(question=question)
-    print(qa_chain.invoke(formatted_prompt)['result'])
+    print(qa_chain.invoke(formatted_prompt)["result"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(main)
