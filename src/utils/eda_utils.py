20a3fc75069a47b920eba1e4e41c866812a16147
Mikolaj <mikolajczachorowski260203@gmail.com>
2024-09-14 22:52:28 +0200

    update examples/eda

--- src/utils/eda_utils.py
+++ src/utils/eda_utils.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
-
-def count_no_papers(file_path: str = "../data/papers_data.csv"):
+def count_no_papers(file_path: str = "../data/papers_data_2.csv"):
     df = pd.read_csv(file_path)
     return df["research papers"].astype(str).apply(lambda x: x.count("{}")).sum()
+
+def count_no_theses(file_path: str = "../data/papers_data_2.csv"):
+    df = pd.read_csv(file_path)
+    return df["Supervisor\'s Theses"].astype(str).apply(lambda x: x.count("{}")).sum()
+
+def count_no_papers_n_theses(file_path: str = "../data/papers_data_2.csv") -> int:
+    df = pd.read_csv(file_path)
+    
+    no_papers_count = df[
+        (df["research papers"].astype(str) == '{}') &
+        (df["Supervisor\'s Theses"].astype(str) == '{}')
+    ].shape[0]
+    
+    return no_papers_count
