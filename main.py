d82a30e12fd765099b6801f7db40a6ab9589e29a
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2024-11-30 17:01:24 +0100

    doc: example post update

--- main.py
+++ main.py
@@ -5,27 +5,6 @@ from pydantic import BaseModel
 from src.components.chains import qa_chain
 
 
-# app = FastAPI()
-
-
-# class DetectionRequest(BaseModel):
-#     data: str
-
-
-# @app.post("/recommend")
-# async def recommend(data: DetectionRequest):
-#     formatted_prompt = PROMPT_TEMPLATE.format(question=data.data)
-#     output = qa_chain.invoke(formatted_prompt)["result"]
-
-#     response = {"response": output}
-
-#     return response
-
-
-# @app.get("/health")
-# async def health():
-#     return {"status": "Healthy"}
-
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
