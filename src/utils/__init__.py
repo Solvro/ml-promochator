9e60f20202e865d5b0ed984a9c05e5efc292c50f
Micz26 <mikolajczachorowski260203@gmail.com>
2024-12-02 14:39:52 +0100

    remove utils from src (not utilized in docker)

--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.scraping_utils import scrape_to_csv, add_apd_data
-from utils.eda_utils import count_no_papers, count_no_theses, count_no_papers_n_theses
-from utils.data_processing_utils import split_csv_by_empty_papers
-
-__all__ = ["scrape_to_csv", "add_apd_data", "count_no_papers", "split_csv_by_empty_papers", "count_no_theses", "count_no_papers_n_theses"]
