ead1a97b34566f5472bb14ff72fdcfd669a633e7
Wojciech Kosmalski <jawmen999@gmail.com>
2025-01-08 22:18:15 +0100

    refactor: don't use openai api; simplify code for test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, HTTPException, Body, Request
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-from src.graph import recommendation_graph
-from src.components.models import InputRecommendationGeneration
+# from src.graph import recommendation_graph
+# from src.components.models import InputRecommendationGeneration
 
 limiter = Limiter(key_func=get_remote_address)
 
@@ -17,11 +17,12 @@ app.state.limiter = limiter
 
 
 @app.post("/recommend/invoke")
-@limiter.limit("1/minute")
+@limiter.limit("1/5second")
 async def invoke(
     request: Request,
     body: dict = Body(..., description="Input JSON"),
 ):
+    print(f"request.client = {request.client}")
     try:
         input_data = body.get("input", {})
         if not input_data:
@@ -29,16 +30,17 @@ async def invoke(
                 status_code=422, detail="Missing 'input' field in the request body."
             )
 
-        request_data = InputRecommendationGeneration(**input_data)
+        # request_data = InputRecommendationGeneration(**input_data)
 
-        final_state = await recommendation_graph.ainvoke(
-            {
-                "faculty": request_data.faculty,
-                "question": request_data.question,
-            }
-        )
+        # final_state = await recommendation_graph.ainvoke(
+        #     {
+        #         "faculty": request_data.faculty,
+        #         "question": request_data.question,
+        #     }
+        # )
 
-        return {"output": final_state["recommendation"]}
+        # return {"output": final_state["recommendation"]}
+        return {"output": "request successfull"}
 
     except HTTPException:
         raise
