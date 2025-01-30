845f59b453cf96b7cdbbd6f0e503e207978d492a
Wojciech Kosmalski <jawmen999@gmail.com>
2025-01-30 02:09:41 +0100

    refactor: revert changes

--- main.py
+++ main.py
@@ -5,26 +5,17 @@ from ipaddress import IPv4Address
 from src.graph import recommendation_graph
 from src.components.models import InputRecommendationGeneration
 import logging
-import re
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 def my_get_ipaddr(request: Request):
-    x = "x-forwarded-for".encode("utf-8")
-    for header in request.headers.raw:
-        if header[0] == x:
-            logger.info("Find out the forwarded-for ip address")
-            origin_ip = re.split(", ", header[1].decode("utf-8"))
-            logger.info(f"origin_ip:\t{origin_ip}")
-            # print(f"forward_ip:\t{forward_ip}")
-            return origin_ip
-    # x_forwarded_for = request.headers.get("X-Forwarded-For")
-    # if x_forwarded_for:
-    #     logger.info(f"X-Forwarded-For header: {x_forwarded_for}")
-    #     logger.info(f"Request from based on X-Forwarded-For header: {x_forwarded_for}")
-    #     return request.headers.get("X-Forwarded-For")
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        logger.info(f"X-Forwarded-For header: ${x_forwarded_for}")
+        logger.info(f"Request from based on X-Forwarded-For header: {x_forwarded_for}")
+        return request.headers.get("X-Forwarded-For")
     host = request.client.host
     if host:
         logger.info(f"Request from based on Host header: {host}")
