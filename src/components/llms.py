6af46ebd97e55be7890c15160bc71f5ad587059f
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2025-03-16 11:03:03 +0100

    refactor: unused files delete

--- src/components/llms.py
+++ src/components/llms.py
@@ -1,12 +1,7 @@
-from langchain_core.runnables import ConfigurableField
-from langchain_openai import ChatOpenAI, OpenAI
+from langchain_openai import ChatOpenAI
 
 from src.components.constants import OPEN_AI_API_KEY
 from src.components.models import Recommendation
-
-llm_openai = OpenAI(temperature=0.2, api_key=OPEN_AI_API_KEY)
-
-llm = llm_openai.configurable_alternatives(ConfigurableField(id='llm'), default_key='openai')
 
 chat_llm = ChatOpenAI(model='gpt-4o-mini', api_key=OPEN_AI_API_KEY, temperature=0.3, max_tokens=5000)
 chat_llm_with_structured = chat_llm.with_structured_output(Recommendation)
